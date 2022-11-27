8668eb3ba6b694f0bfdfb6524cf735505a5e06df
Pushpalal-Tharu <pushpalaltharu1@gmail.com>
2022-11-27 11:12:13 +0545

    dart model class is generated using plugin

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_rest_api/models/posts_model.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +12,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postlist = [];
+  Future<List<PostsModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      postlist.clear();
+      for (Map i in data) {
+        postlist.add(PostsModel.fromJson(i));
+      }
+      return postlist;
+    } else {
+      return postlist;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +35,57 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Rest API"),
       ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                      child: CircularProgressIndicator(
+                    strokeWidth: 3,
+                  ));
+                } else {
+                  return ListView.builder(
+                      itemCount: postlist.length,
+                      itemBuilder: (context, index) {
+                        // return Text(index.toString());
+                        // return Text(postlist[index].title.toString());
+
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Title",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(postlist[index].title.toString()),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  "Description",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(postlist[index].body.toString()),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
